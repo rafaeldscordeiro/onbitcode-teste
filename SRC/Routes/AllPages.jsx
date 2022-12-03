af217242bfb1c512fd75030b74ca32bbafbf30cd
rafaeldscordeiro <rafael.dscordeiro@gmail.com>
2022-12-03 16:06:28 -0300

    Estrutura de páginas e rotas

---
+++ SRC/Routes/AllPages.jsx
@@ -0,0 +1,21 @@
+import {NavigationContainer, StackActions} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Start from "../pages/Start/Index";
+
+const stackp = createNativeStacknavigator();
+
+export default function AllPages(){
+return{
+        <NavigationContainer>
+        <StackActions.NavigationContainer
+         screenOptions={{
+            headerShown:false,
+         }}>
+         <StackActions.Scre>
+         </>
+        
+        </NavigationContainer>
+}
+
+}
---
+++ SRC/pages/Start/Index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, ScrollView,Text, Image } from "react-native";
+//import { ScrollView } from "react-native-gesture-handler";
+
+export default function Start(){
+    return{
+        <View>
+         <ScrollView showsHorizontalScrollIndicator={false}>
+         <View>
+          <image source={require( "../../Asserts/icons/logo3.png")}
+            <Text>
+            Vamos transformar sua vida {"/n"} em jogo, buscando sempre {"/n"} o melhor nivel.
+            </Text>
+            </View>
+         </ScrollView>
+         </view>
+    }
+    }
+}
